c8ea2ac3f44f0549d94164978a3fed76a2d9e788
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-19 17:08:59 +0400

    Add docs to Location data holder

--- src/main/java/com/slateblua/locator/Location.java
+++ src/main/java/com/slateblua/locator/Location.java
@@ -4,10 +4,21 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Represents a geographic location with latitude and longitude coordinates.
+ */
 @AllArgsConstructor
 @Data
 @NoArgsConstructor
 public class Location {
+
+    /**
+     * The latitude of the location in decimal degrees.
+     */
     private double latitude;
+
+    /**
+     * The longitude of the location in decimal degrees.
+     */
     private double longitude;
 }
